4efb658313515e5b0121c78898b016ba5218118f
Mike Bauer <mwbauer@amazon.com>
2025-12-26 15:37:18 -0800

    fix: Compat

--- compile_mac.sh
+++ compile_mac.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 "/Users/Shared/Epic Games/UE_5.7/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun \
-  -project="/Users/mwbauer/Dev/sandbox/Sandbox/Sandbox.uproject" \
+  -project="/Users/$USER/Dev/sandbox/Sandbox/Sandbox.uproject" \
   -platform=Mac \
   -clientconfig=Development \
   -build -cook -stage -package -archive \
-  -archivedirectory="/Users/mwbauer/Dev/sandbox/Sandbox/Build" \
+  -archivedirectory="/Users/$USER/Dev/sandbox/Sandbox/Build" \
   -unattended \
   -utf8output \
   -IgnoreCookErrors
